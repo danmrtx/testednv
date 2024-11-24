b12cfa38ca082a96cd48548713447f57377cef5a
Daniel Martins <143227346+danmrtx@users.noreply.github.com>
2024-11-24 15:49:58 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const avaliacoes = await db.collection('avaliacoes').find().limit(100).toArray();
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ObjectId } = require('mongodb');
